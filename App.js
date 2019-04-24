c6830f30580abfb314466c25e9df88f52bef1f79
Borek-23 <23.svejda@gmail.com>
2019-04-24 11:01:11 +0100

    adding persistent storage as part of the assessment

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {AsyncStorage} from 'react-native';
 
 export default class App extends Component {
-  render() {
-    return (
-        <View style={styles.container}>
-          <Text>Welcome to React Native!</Text>
-        </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            quickNote: ''
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Welcome to React Native!</Text>
+                <Button title={'Make a Quick Note'} onPress={this._storeData}/>
+                <Button title={'Reveal your Quick Note'} onPress={this._storeData}/>
+
+                <View >
+                    <Button title={'Go to your To-Do Lists'} onPress={this._storeData}/>
+                </View>
+            </View>
+        );
+    }
+
+    // Persistent storage using AsyncStorage component
+    _storeData = async () => {
+        try {
+            await AsyncStorage.setItem('quickNote', {note: this.setState(this.state.quickNote)});
+            alert('Note Saved');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // Retrieve data from persistent storage
+    _retrieveData = async () => {
+        try {
+            const note = await
+        } catch (error) {
+            console.error(error);
+        }
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 });
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 
-// import App from './App';
+import App from './App';
 import BasicFlatList from './components/BasicFlatList'
 import {name as appName} from './app.json';
 
 
 
-AppRegistry.registerComponent(appName, () => BasicFlatList);
+AppRegistry.registerComponent(appName, () => App); // BasicFlatList
